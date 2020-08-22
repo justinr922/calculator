8db8a13c7470f021654ae5a9e551cbbc9fb2a9f7
JustinRyan <justinr922@gmail.com>
2020-08-22 16:58:54 -0400

    Added Operator functionalities

--- calculator.js
+++ calculator.js
@@ -1,7 +1,8 @@
 let workingMemory = '0';
 let savedMemory = '';
-let memoryOperator = '';
+let storedOperator = '';
 let display = document.querySelector('#display')
+displayUpdate();
 
 
 
@@ -20,7 +21,7 @@ const clearButton = document.querySelector('.clear')
 clearButton.addEventListener('click', () => {
     workingMemory = '0';
     savedMemory =  '';
-    memoryOperator = '';
+    storedOperator = '';
     displayUpdate();
 })
 
@@ -32,6 +33,80 @@ modifierButtons.forEach((button) => {
         displayUpdate();
     })
 })
+
+// Factorial functionality
+const factorialButton = document.querySelector('#factorial');
+factorialButton.addEventListener('click', () => {
+    factorial();
+    displayUpdate();
+    workingMemory = '0';
+})
+
+// Operator functionality
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach((button => {
+    button.addEventListener('click', () => {
+        if ((storedOperator && savedMemory) || button.textContent == '='){
+            operate(storedOperator);
+            if (button.textContent == '=') {
+                storedOperator = '';
+            }
+            displayUpdate();
+        }
+
+
+
+        saveInMemory();
+
+        if (button.textContent != '=' || button.textContent != '!') {
+            storedOperator = button.textContent;
+            workingMemory = '0'
+        }
+    })  
+}))
+
+function operate(action) {
+    let numSavedMemory = parseFloat(savedMemory);
+    let numWorkingMemory = parseFloat(workingMemory);
+    
+    if (action == '') return
+    else if (action == '+') {
+        workingMemory = (numSavedMemory + numWorkingMemory).toString();
+    } else if (action == '-') {
+        workingMemory = (numSavedMemory - numWorkingMemory).toString();
+    } else if (action == '*') {
+        workingMemory = (numSavedMemory * numWorkingMemory).toString();
+    } else if (action == '/') {
+        if (numWorkingMemory === 0) {
+            alert('Error. Divide by zero')
+            return
+        };
+        workingMemory = (numSavedMemory / numWorkingMemory).toString();
+    }
+}
+
+function factorial() {
+    let numWorkingMemory = parseFloat(workingMemory);
+    if (numWorkingMemory % 1 != 0) {
+        alert('Error. Factorial only works on integers.')
+        return
+    } else if (numWorkingMemory < 0) {
+        alert('Error. Factorial only works on non negatives.')
+        return
+    }
+
+    if (numWorkingMemory == 0 || numWorkingMemory == 1) {
+        workingMemory = '1';
+    } else {
+        for (let index = numWorkingMemory - 1; index > 1; index--) {
+            numWorkingMemory = numWorkingMemory * index;
+        }
+
+        if (numWorkingMemory > 1e10) {
+        workingMemory = numWorkingMemory.toExponential(6)
+        } else {workingMemory = numWorkingMemory.toString()}
+    }
+}
 
 function updateCurrentMemory(button) {
     let classes = button.classList;
@@ -49,7 +124,7 @@ function modifyWorkingMemory(button) {
         workingMemory = numericalRep.toString()
     } else if (button.textContent == '+/-') {
         if (workingMemory == '0') return
-        
+
         if (workingMemory.split('')[0] == '-') {
             workingMemory = workingMemory.substring(1);
         } else {workingMemory = '-' + workingMemory;}
@@ -59,7 +134,6 @@ function modifyWorkingMemory(button) {
 function saveInMemory() {
     savedMemory = workingMemory;
 }
-
 
 function displayUpdate() {
     display.value = workingMemory;
